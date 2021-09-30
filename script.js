72d0ad1b92fa5f27b3bff9c4e13c95ea0c29a39f
babingthon <babingthon.bandeira@gmail.com>
2021-09-29 22:31:53 -0300

    .

---
+++ script.js
@@ -0,0 +1,23 @@
+
+async function buscarCep(e) {
+
+
+
+  let cep = document.getElementById('cep').value;
+  let req = `https://viacep.com.br/ws/${cep}/json/`  
+  let resp = await fetch(req);
+  let result = await resp.json();
+
+  montarDados(result);
+    
+}
+
+function montarDados(json) {
+  for (let i in json) {
+    document.getElementById("logradouro").innerHTML = "Endereço: " + json.logradouro;
+    document.getElementById("cidade").innerHTML = "Cidade: " + json.localidade;
+    document.getElementById("ibge").innerHTML = "Cód. IBGE: " + json.ibge;
+    document.getElementById("bairro").innerHTML = "Bairro: " + json.bairro;
+    document.getElementById("uf").innerHTML = "Estado: " + json.uf;
+  }
+}
